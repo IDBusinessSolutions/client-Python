b04ddc93868f641db9d93851926060e4368d4863
Tim Hayward <thayward@idbs.com>
2020-10-16 09:42:44 +0100

    DS-1687 removing unused import

--- build.py
+++ build.py
@@ -2,30 +2,29 @@ import setup
 import shutil
 import os
 import glob
-import robot.libdoc as libdoc
 
-#name and package dir
+# name and package dir
 library_name = 'reportportal_client'
 packages_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Packages'))
 
-#version
+# version
 file = os.path.join(os.path.dirname(__file__), library_name, 'version.py')
 exec(open(file).read())
 
-#generate the package
+# generate the package
 setup.run_setup(VERSION, ['sdist'])
 
-#copy package to packages directory
+# copy package to packages directory
 doc_files = glob.iglob(os.path.join(os.path.dirname(__file__), 'dist/*.*'))
 for file in doc_files:
     if os.path.isfile(file):
         shutil.copy(file, packages_dir)
         
-#tidy up build folders
+# tidy up build folders
 shutil.rmtree('dist')
 egg_info_folders = glob.iglob(os.path.join(os.path.dirname(__file__), 'src/*.egg-info'))
 for folder in egg_info_folders:
     shutil.rmtree(folder)
     
-#build confirmation message
+# build confirmation message
 print("Build successful")
